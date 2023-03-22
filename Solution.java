4ea927ae697d6a01e00744d16514d6c20d1a9c3a
pleaseMe8282 <128647796+pleaseMe8282@users.noreply.github.com>
2023-03-23 07:17:42 +0900

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,55 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Solution {
+    public static int solution(String s) {
+        int answer = 0;
+        if(s.contains("zero")){
+            s = s.replaceAll("zero", "0");
+                //바꿔주는게 아니라 바꾼걸 반 환 해 주 는 것
+        }
+        if(s.contains("one")){
+            s = s.replaceAll("one", "1");
+
+        }
+        if(s.contains("two")){
+            s = s.replaceAll("two", "2");
+
+        }
+        if(s.contains("three")){
+            s = s.replaceAll("three", "3");
+
+        }
+        if(s.contains("four")){
+            s = s.replaceAll("four", "4");
+
+        }
+        if(s.contains("five")){
+            s = s.replaceAll("five", "5");
+
+        }
+        if(s.contains("six")){
+            s = s.replaceAll("six", "6");
+
+        }
+        if(s.contains("seven")){
+            s = s.replaceAll("seven", "7");
+
+        }
+        if(s.contains("eight")){
+            s = s.replaceAll("eight", "8");
+
+        }
+        if(s.contains("nine")){
+            s = s.replaceAll("nine", "9");
+
+        }
+        answer = Integer.parseInt(s);
+        //System.out.println(s);
+        return answer;
+    }
+    public static void main(String[] args) {
+        System.out.println(solution("one4seveneight"));
+    }
+
+}
